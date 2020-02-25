ed463c477a01c731fd9fb38ae64f09f6988d0336
Sydney Barovsky <sbarovsky@gmail.com>
2020-02-25 11:56:26 +0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # READ ALL
+  get '/tasks', to: 'tasks#index', as: 'tasks'
+
+  # READ ONE
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # CREATE
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
+  # UPDATE
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  post 'tasks/:id', to: 'tasks#update'
+
+  # DESTROY
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
