617f4a7d56afa6d89b5a5d950451632c0f975bc3
Sydney Barovsky <sbarovsky@gmail.com>
2020-02-25 16:01:31 +0800

    ugly but working task manager app

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # ROOT
+  # root to: 'tasks#index'
+
   # READ ALL
   get '/tasks', to: 'tasks#index', as: 'tasks'
-
-  # READ ONE
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
 
   # CREATE
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
 
+  # READ ONE
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
   # UPDATE
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  post 'tasks/:id', to: 'tasks#update'
+  patch 'tasks/:id', to: 'tasks#update'
 
   # DESTROY
   delete '/tasks/:id', to: 'tasks#destroy'
